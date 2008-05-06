55fe3b786f2a2be9f4c87abbd1e633f7f1e7b49a
Jason Antman <jason@jasonantman.com>
2008-05-06 20:41:11 +0000

    working...

--- TuxTruck_SkinManager.py
+++ TuxTruck_SkinManager.py
@@ -5,24 +5,54 @@ import wx
 class TuxTruck_SkinManager:
     "Provides interface to all of the skin data"
 
+    # current skin information
     currentSkinName = "" # declare it empty
     currentSkinFile = "" # declare it empty
 
+    # primary (day) color scheme
     bgColor = "" # declare it empty
     fgColor = "" # declare it empty
     highlightColor = "" # declare it empty
+
+    # secondary (night) color scheme
+    night_bgColor = ""
+    night_fgColor = ""
+    night_highlightColor = ""
+
+    # main window settings
     topWindowSize = 0
+    topWindowPos = 0
+    topWindowCentered = 0
+
+    #buttons
+    butn_home_size = (0, 0)
+    butn_home_pos = (0, 0)
+    butn_gps_size = (0, 0)
+    butn_gps_pos = (0, 0)
+    butn_audio_size = (0, 0)
+    butn_audio_pos = (0, 0)
 
     def loadSkin(self, name, file):
         # load the skin with given name at given location
 
-        # DEBUG
+        # DEBUG TEST SETTINGS
         self.currentSkinName = name
         self.currentSkinFile = file
+
         self.bgColor = wx.Colour(22,127,230)
         self.fgColor = wx.Colour(3,90,166)
         self.highlightColor = wx.Colour(211,235,253)
+
         self.topWindowSize = wx.Size(800, 480)
+        self.topWindowPos = wx.Size(100,100)
+        self.topWindowCentered = 1
+
+        self.butn_home_size = (100, 70)
+        self.butn_home_pos = (10, 410)
+        self.butn_gps_size = (100, 70)
+        self.butn_gps_pos = (110, 410)
+        self.butn_audio_size = (100, 70)
+        self.butn_audio_pos = (210, 410)
 
     def __init__(self, parent):
         #get the default skin name and file from settings
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+# Time-stamp: "2008-05-06 16:38:42 jantman"
+
 import wx
 
 # application includes
@@ -10,32 +12,55 @@ class TuxTruck_MainApp(wx.Frame):
     def __init__(self, parent, id):
         wx.Frame.__init__(self, parent, id, '', style=wx.NO_BORDER)
 
+        # variables holding state information
+        self.currentColorScheme = "day"
+        self.currentButton = ""
+
         # setup the main frame
         self.SetPosition(wx.Point(0,0))
         self.SetSize(settings.skin.topWindowSize)
         self.SetBackgroundColour(settings.skin.bgColor)
-        self.CenterOnScreen()
+        if settings.skin.topWindowCentered == 1:
+            self.CenterOnScreen()
         self.SetWindowStyle(wx.NO_BORDER)
 
+        # DEBUG
+        print settings.skin.butn_home_size
+        # END
+
         # setup buttons
-        self.butn_home = wx.Button(self, 1, 'Home', (10, 410), (70, 100))
-        self.butn_gps = wx.Button(self, 2, 'GPS', (120, 410), (70, 100))
-        self.butn_close = wx.Button(self, 3, 'Close', (230, 410), (70, 100))
+        self.butn_home = wx.Button(self, 1, 'Home', settings.skin.butn_home_pos, settings.skin.butn_home_size)
+        self.butn_gps = wx.Button(self, 2, 'GPS', settings.skin.butn_gps_pos, settings.skin.butn_gps_size)
+        self.butn_audio = wx.Button(self, 3, 'Audio', settings.skin.butn_audio_pos, settings.skin.butn_audio_size)
+
+        # DEBUG - close button
+        self.butn_close = wx.Button(self, 4, 'Close', (530, 410), (100, 70))
+        # END DEBUG
 
         # bind buttons
         self.Bind(wx.EVT_BUTTON, self.OnClick_home, id=1)
         self.Bind(wx.EVT_BUTTON, self.OnClick_gps, id=2)
-        self.Bind(wx.EVT_BUTTON, self.OnClose, id=3)
+        self.Bind(wx.EVT_BUTTON, self.OnClick_audio, id=3)
+
+        # DEBUG - close button
+        self.Bind(wx.EVT_BUTTON, self.OnClose, id=4)
+        # END DEBUG
 
     def OnClick_gps(self, event):
         print "GPS clicked"
         self.butn_gps.SetBackgroundColour(settings.skin.fgColor)
         self.Refresh()
+        self.currentButton = "butn_gps"
+
+    def OnClick_audio(self, event):
+        print "Audio clicked"
+        self.currentButton = "butn_audio"
 
     def OnClick_home(self, event):
         print "Home clicked"
         self.SetBackgroundColour('Yellow')
         self.Refresh()
+        self.currentButton = "butn_home"
 
     def OnClose(self, event):
         frame.Close(True)        
